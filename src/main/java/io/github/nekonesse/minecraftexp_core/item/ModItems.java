2de2d492c0e7b10d5d1aefe623b3c583b8223b35
nekonesse <kggkittygeometrygames@gmail.com>
2023-10-04 19:52:19 -0400

    initial commit

---
+++ src/main/java/io/github/nekonesse/minecraftexp_core/item/ModItems.java
@@ -0,0 +1,10 @@
+package io.github.nekonesse.minecraftexp_core.item;
+
+import net.minecraft.item.Item;
+
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
+
+public class ModItems {
+
+	public static final Item COPPER_NUGGET = new Item(new QuiltItemSettings());
+}
